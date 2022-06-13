5a4e5ab58387b88416fd5dd7a983504ccc7e4776
TravisCossarini <traviscossarini@gmail.com>
2022-06-13 08:46:15 -0400

    June 13

---
+++ prototypes-01-starting-setup/app.js
@@ -0,0 +1,14 @@
+class Person {
+    name = 'Max';
+
+    constructor() {
+        this.age = 30;
+    }
+
+    greet() {
+        console.log("Greet")
+    }
+}
+
+const person = new Person();
+person.greet();
